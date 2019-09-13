3e8200fb1fa27bb818cbcaae9692e8158c94b996
Mayank <agrawal.mayank1996@gmail.com>
2019-09-13 18:00:13 +0530

    Added  LinkedList

---
+++ linkedlist.rb
@@ -0,0 +1,70 @@
+class Node
+	attr_accessor :value ,:next
+	def initialize(value)
+		@value=value
+		@next=nil
+		return Node
+	end
+end
+
+class LinkedList
+	attr_accessor :head
+	def initialize
+		@head=nil
+	end
+
+	def add(val)
+		if @head == nil
+			@head = Node.new(val)
+		else
+			curr=@head
+			while curr.next != nil
+				curr=curr.next
+			end
+			curr.next = Node.new(val)
+		end
+	end
+
+	def delete(val)
+		if @head.value == val
+			@head=nil
+		else
+			curr=@head
+			while curr.next != nil 
+				if(curr.next.value==val)
+					curr.next=curr.next.next 
+					break
+				else
+					curr=curr.next
+				end
+			end
+
+		end 
+	end
+
+	def disp()
+		puts @head
+		curr=@head
+		while curr != nil
+			puts curr.value
+			curr=curr.next
+		end
+	end
+
+end
+
+a=LinkedList.new
+a.add(1)
+a.add(2)
+a.add(3)
+a.add(4)
+a.add(5)
+a.add(6)
+a.delete(2)
+a.disp
+
+
+
+
+
+
