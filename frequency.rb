e23c7ea996330f5f62335cd7f1f3ecaf0cd74707
Mayank <agrawal.mayank1996@gmail.com>
2019-09-13 15:10:54 +0530

    Used the each_char method to find the frequency

--- frequency.rb
+++ frequency.rb
@@ -1,17 +1,15 @@
 puts "Enter a atring"
 a=gets.chomp
 b={}
-i=0
-while i < a.length do 
+a.each_char do |c|
 
-	if b.key?(a[i])==false
-		b=b.merge({a[i]=>1})
+	if b.key?(c)==false
+		b=b.merge({c=>1})
 		puts "here"
 	else
-		b[a[i]]=b[a[i]]+1
+		b[c]=b[c]+1
 
 	end
-	i += 1
 end
 
 puts b
